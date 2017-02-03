5806553984ce629ae3282bed9aaa1363565fa4d6
Christopher <chamaya@uci.edu>
2017-02-02 18:43:19 -0800

    second UI update

--- Senior_Design/app/src/main/java/com/example/anthonydelarosa/senior_design/HomePage.java
+++ Senior_Design/app/src/main/java/com/example/anthonydelarosa/senior_design/HomePage.java
@@ -2,17 +2,22 @@ package com.example.anthonydelarosa.senior_design;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 
 public class HomePage extends AppCompatActivity {
 
@@ -21,14 +26,50 @@ public class HomePage extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
-
+    TextView dateType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
+
+        Bundle extras = getIntent().getExtras();
+        String myEtText;
+        TextView strengthType = (TextView) findViewById(R.id.strengthType);
+        TextView teaType = (TextView) findViewById(R.id.teaType);
+        dateType = (TextView) findViewById(R.id.time_left);
+        if (extras != null) {
+            myEtText = extras.getString("strength");
+            strengthType.setText(myEtText);
+            myEtText = extras.getString("tea");
+            teaType.setText(myEtText);
+            long myEtLong = extras.getLong("date");
+            final CounterClass timer = new CounterClass(myEtLong, 1000);
+            timer.start();
+
+        }
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    public class CounterClass extends CountDownTimer{
+        public CounterClass(long millisInFuture, long countDownInterval){
+            super (millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished){
+            long millis = millisUntilFinished;
+            String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
+                    TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
+                    TimeUnit.MILLISECONDS.toSeconds((millis)) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+            dateType.setText(hms);
+        }
+
+        @Override
+        public void onFinish(){
+            dateType.setText("00:00:00");
+        }
+
     }
 
     /**
@@ -49,9 +90,11 @@ public class HomePage extends AppCompatActivity {
     public void toSettings(View view){
         Intent next = new Intent(this, Settings.class);
         startActivity(next);
+        finish();
     }
     public void EndApp(View view){
         finish();
+        System.exit(0);
     }
 
     @Override
